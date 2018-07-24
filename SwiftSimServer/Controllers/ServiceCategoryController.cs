53fafeae411b15a814d69df2ddd52467788195bb
Fang, Huai An <liddle.fang@gmail.com>
2018-07-24 23:10:38 +0800

    add servicecategory add static files

---
+++ SwiftSimServer/Controllers/ServiceCategoryController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SwiftSimServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class ServiceCategoryController : Controller
+    {
+
+        // https://localhost:5001/api/servicecategory
+        [HttpGet]
+        public IEnumerable<ServiceCategory> Get()
+        {
+            return ReadFromDatabase();
+        }
+
+        // https://localhost:5001/api/servicecategory/0
+        [HttpGet("{id}")]
+        public ServiceCategory Get(int id)
+        {
+            var results = ReadFromDatabase();
+
+            if( results.Count > id){
+                return results[id];
+            }
+
+            return results[results.Count-1];
+        }
+
+        private List<ServiceCategory> ReadFromDatabase(){
+            var results = new List<ServiceCategory>();
+
+            results.Add(new ServiceCategory
+            {
+                Name = "",
+                ImagePath = "https://swift.azurewebsites.net/Images/cafe.jpg",
+                Index = 0
+            });
+
+
+            return results;
+        }
+
+    }
+
+    public class ServiceCategory
+    {
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+        public int Index { get; set; }
+    }
+}
--- SwiftSimServer/Startup.cs
+++ SwiftSimServer/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.FileProviders;
 
 namespace SwiftSimServer
 {
@@ -40,8 +35,17 @@ namespace SwiftSimServer
                 app.UseHsts();
             }
 
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                Path.Combine(Directory.GetCurrentDirectory(), "Images")),
+                RequestPath = "/Images"
+            });
+
             app.UseHttpsRedirection();
             app.UseMvc();
         }
+
     }
 }
